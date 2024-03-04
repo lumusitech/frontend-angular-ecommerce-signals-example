fcba51f93a7db06d32db6af14a4e2b50498409be
lumusitech <lumusitech@gmail.com>
2024-03-04 12:32:55 -0300

    format: add format with 2 spaces for each tab

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-angular-v17-demo-store');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, frontend-angular-v17-demo-store'
+    );
   });
 });
