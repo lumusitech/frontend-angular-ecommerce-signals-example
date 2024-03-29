a2f75007611990d31bbc9e096fe575536b28a3ce
lumusitech <lumusitech@gmail.com>
2024-03-29 13:17:28 -0300

    feat: add star rating

--- src/app/api/products.service.ts
+++ src/app/api/products.service.ts
@@ -1,5 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import {
+  EnvironmentInjector,
+  Injectable,
+  inject,
+  runInInjectionContext,
+  signal,
+} from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { environment } from '@envs/environment.development';
 import type { Product } from '@shared/models/product.interface';
 
@@ -10,6 +17,7 @@ export class ProductsService {
   public products = signal<Product[]>([]);
   private readonly _http = inject(HttpClient);
   private readonly _endpoint = environment.apiUrl;
+  private readonly _injector = inject(EnvironmentInjector);
 
   constructor() {
     this.getProducts();
@@ -23,6 +31,10 @@ export class ProductsService {
   }
 
   public getProductById(id: number) {
-    return this._http.get<Product>(`${this._endpoint}/products/${id}`);
+    return runInInjectionContext(this._injector, () =>
+      toSignal<Product>(
+        this._http.get<Product>(`${this._endpoint}/products/${id}`),
+      ),
+    );
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { errorResponseInterceptor } from '@shared/interceptors/error-response.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())],
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([errorResponseInterceptor]),
+    ),
+  ],
 };
